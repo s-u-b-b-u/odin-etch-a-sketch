9f5f543603fd861a72b3141b5789677b318070cc
Subbu <rsubbu931@gmail.com>
2026-05-21 13:27:27 +0530

    Add n by n grid according to user input

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const gridContainer = document.querySelector('.container');
+const btn = document.querySelector('button');
+
+
+// step 1 to create n by n grid 
+
+let gridSize;
+
+btn.addEventListener('click', ()=> {
+
+    
+
+    gridSize = prompt ("Enter size;");
+    for (let i = 0; i < gridSize; i++) {
+        const row1 = document.createElement('div');
+        row1.classList.add('row');
+    
+        for (let j = 0; j < gridSize; j++) {
+            const column = document. createElement('div');
+            column.classList.add('column');
+            row1.appendChild(column);
+        }
+        
+        gridContainer.appendChild(row1);
+    
+    }
+        
+})
+console.log(gridSize)
+
+
+
+    
